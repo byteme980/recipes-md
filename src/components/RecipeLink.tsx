61bd439955543bd7a314414ec330082f1ccfff57
philiprlarie <philiprlarie@gmail.com>
2021-05-16 19:51:46 -0400

    Update src/components/RecipeLink.tsx
    Co-authored-by: Kim Strauch <byteme980@gmail.com>

--- src/components/RecipeLink.tsx
+++ src/components/RecipeLink.tsx
@@ -4,7 +4,6 @@ import Img from "gatsby-image"
 
 const RecipeLink = ({ recipe }) => (
   <div className="recipe-link">
-    {/* might need to change to a normal img tag */}
     {recipe.frontmatter.imageUrl && <img src={recipe.frontmatter.imageUrl} />}
     <Link to={recipe.frontmatter.slug}>
       {recipe.frontmatter.title}
